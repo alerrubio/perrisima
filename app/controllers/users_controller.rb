e5de4440fdaddbdbbdb7afb8237919910c11ec0c
Ale <alejandra.rodriguezub@uanl.edu.mx>
2023-04-04 22:34:57 -0600

    Users controller done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,52 @@
 class UsersController < ActionController::API
   def index
     @users = User.all
+
+    render json: @users.to_json
   end
 
   def create
     @user = User.new(user_params)
+
     if @user.save
       render json: @user, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :not_found
+    else
+      render json: @user, status: :ok
+    end
+  end
+
+  def update
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :not_found
+    else
+      if @user.update(user_params)
+        render json: @user, status: :ok
+      else
+        render json: { data: 'Could not update user', error: @user.errors }, status: :unprocessable_entity
+      end
+    end
+  end
+
+  def destroy
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found' }, status: :not_found
+    else
+      @user.destroy
+      render json: { message: "User deleted" }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get "/users", to: "users#index"
-  post "/users", to: "users#create"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users
+  #get "/users", to: "users#index"
+  #post "/users", to: "users#create"
+  #get "/users/:id", to: "users#show"
 end
