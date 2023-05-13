1d4742e1b804704a13cb0c19844a74395bc3d8ba
Ale <alejandra.rodriguezub@uanl.edu.mx>
2023-05-13 02:32:53 -0600

    no se

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ActionController::API
 
   def show
     begin
-      @user = User.find(params[:id])
+      @user = User.where(email: params[:email])
     rescue ActiveRecord::RecordNotFound
       render json: { error: 'User not found' }, status: :not_found
     else
