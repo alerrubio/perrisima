f463b513aef8dc8d3e7a180eaa66b15231b27a88
PeterGarza12 <pedro.garzagnz@uanl.edu.mx>
2023-05-16 14:48:51 -0600

    al final no hice cambio

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-class ClientsController < ActionController::API
-  def index
-    @clients = Client.all
-    render json: @clients.to_json
-  end
-
-  def show
-    begin
-      @client = Client.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Client not found' }, status: :not_found
-    else
-      render json: @client, status: :ok
-    end
-  end
-
-  def create
-    @client = Client.new(client_params)
-    if @client.save
-      render json: @client, status: :created
-    else
-      render json: @client.errors, status: :unprocessable_entity
-    end
-  end
-
-  def update
-    begin
-      @client = Client.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Client not found' }, status: :not_found
-    else
-      if @client.update(client_params)
-        render json: @client, status: :ok
-      else
-        render json: { data: 'Could not update client', error: @client.errors }, status: :unprocessable_entity
-      end
-    end
-  end
-
-  def destroy
-    begin
-      @client = Client.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'Client not found' }, status: :not_found
-    else
-      @client.destroy
-      render json: { message: "Client deleted" }
-    end
-  end
-
-  private
-
-  def client_params
-    params.require(:client).permit(:name, :phone_number)
-  end
-
-end
-=======
 class ClientsController < ActionController::API
   def index
     @clients = Client.all
@@ -128,4 +69,3 @@ class ClientsController < ActionController::API
   end
 
 end
->>>>>>> 29935e7e9d245752e38869ed1b48ce69436eecad
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-class Client < ApplicationRecord
-    validates :name, 
-    presence: true,
-    format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/, message: "Invalid name" }
-
-end
-=======
 class Client < ApplicationRecord
     validates :name, 
     presence: true,
@@ -14,4 +6,3 @@ class Client < ApplicationRecord
     presence: true, 
     format: { with: /(\A[\d]{10}\z)/ }
 end
->>>>>>> 29935e7e9d245752e38869ed1b48ce69436eecad
